cce1722655daf8942a826a6f6d9e73381afec643
Ginger Williams <gingerwilliams@MacBook-Pro-2.local>
2025-03-17 12:19:38 -0400

    Appended PATCH call with Update analysis Connected Analysis data with Entry Update Data in prisma by Editor Component

--- journal/app/(dashboard)/journal/[id]/page.tsx
+++ journal/app/(dashboard)/journal/[id]/page.tsx
@@ -12,6 +12,9 @@ const getEntry = async (id) => {
         where: {
             id,
             userId: user.id
+        },
+        include: {
+            analysis: true
         }
     })
 
@@ -22,37 +25,10 @@ const getEntry = async (id) => {
 const EntryPage = async ({ params }) => {
     const p = await params
     const entry = await getEntry(p.id)
-    const analysisData = [
-        {name: "Summary", value: ""},
-        {name: "Subject", value: ""},
-        {name: "Mood", value: ""},
-        {name: "Positive", value: "true"}
-    ]
+
     return (
-        <div className="h-full w-full grid grid-cols-3">
-            <div className="col-span-2">
-                <Editor entry={entry}/>
-            </div>
-            <div className="border-l border-black/10" >
-                <div className="bg-blue-300 px-6 py-10">
-                    <h2 className="text-2xl">Analysis</h2>
-                </div>
-                <div>
-                        <ul>
-                            {
-                                analysisData.map(item => (
-                                    <li
-                                        className="px-2 py-4 flex items-center justify-between border-b border-t border-black/10"
-                                        key={item.name}
-                                    >
-                                        <span className="text-lg font-semibold">{ item.name }</span>
-                                        <span>{ item.value }</span>
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </div>
-            </div>
+        <div className="h-full w-full">
+            <Editor entry={entry}/>
         </div>
     )
 }
--- journal/components/Editor.tsx
+++ journal/components/Editor.tsx
@@ -7,22 +7,60 @@ import { useAutosave } from "react-autosave";
 const Editor = ({ entry }) => {
     const [value, setValue] = useState(entry.content)
     const [isLoading, setIsLoading] = useState(false)
+    const [analysis, setAnalysis] = useState(entry.analysis)
+    const { summary, subject, color, mood, positive} = analysis;
+
+    const analysisData = [
+        {name: "Summary", value: summary},
+        {name: "Subject", value: subject},
+        {name: "Mood", value: mood},
+        {name: "Positive", value: JSON.stringify(positive)}
+    ]
+
     useAutosave({
         data: value,
         onSave: async (_value) => {
             setIsLoading(true)
-            const updated = await updateEntry(entry.id, _value) // is _value the current or prevValue
+            const { analysis } = await updateEntry(entry.id, _value) // is _value the current or prevValue
+            console.log("updated", analysis)
+            setAnalysis(analysis)
             setIsLoading(false)
         } 
     })
+
     return (
-        <div className="w-full h-full">
-            {isLoading && <div>Loading... </div>}
-            <textarea
-                className="w-full h-full p-8 text-xl"
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-            />
+        <div className="w-full h-full grid grid-cols-3">
+            <div className="col-span-2">
+                {isLoading && <div>Loading... </div>}
+                <textarea
+                    className="w-full h-full p-8 text-xl"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                />
+            </div>
+            <div className="border-l border-black/10" >
+                <div 
+                    className="px-6 py-10"
+                    style={{ backgroundColor: color }}
+                >
+                    <h2 className="text-2xl">Analysis</h2>
+                </div>
+                <div>
+                    <ul>
+                        {
+                            analysisData.map(item => (
+                                <li
+                                    className="px-2 py-4 flex items-center justify-between border-b border-t border-black/10"
+                                    key={item.name}
+                                >
+                                    <span className="text-lg font-semibold">{ item.name }</span>
+                                    <span>{ item.value }</span>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
